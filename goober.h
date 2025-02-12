4d80bd48bcb7fd64d7af1801ca7b5970007e0bbb
Eddie2988 <165416446+Eddie2988@users.noreply.github.com>
2025-02-11 16:16:05 -0800

    Update goober.h

--- goober.h
+++ goober.h
@@ -6,11 +6,12 @@ class Goober {
         int health;
     public:
          Goober() : health(20) {}
+         bool isAlive() const {return health > 0;}
          void takeDamage(unsigned int amount) 
-        {
-            health -= amount;
-            std::cout << "You took " << amount << " damage.\n";
-            std::cout << "Health: " << health << ".\n";
-        }
+         {
+               health -= amount;
+               std::cout << "You took " << amount << " damage.\n";
+               std::cout << "Health: " << health << ".\n";
+         }
 
 };
